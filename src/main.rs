95167a9ebd2274934dfff5e4542b2a463c079693
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-05-27 21:39:38 +1000

    feat/perf: add new method for performing archive
    Better performance via copy + check + delete than
    attempting to performance renames/moves

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,22 @@
 
 // Add tests
 
+// Additions: ----------
+// ie: a folder must contain min. 800 files to even be considered > Okay we have our list of things,
+// create a spawned thread for each (via tokio::spawn() as we don't care when each finishes and
+// need to run through all of them regardless of order (Maybe rayon??)
+//
+// then for each spawned thread(ie: each dir that's valid) - go through and spawn N tasks to get metadata,
+// then moved the file
+// (Where N is the number of files in the folder)
+// Potential cons
+//
+// We could end up with severe backpressure, or the system could run out of resources?
+
 mod error;
 mod file_handling;
 mod parser;
 mod prelude;
-
-use std::path::PathBuf;
 
 pub use self::prelude::{Error, Result, W};
 
@@ -26,26 +36,33 @@ async fn main() -> Result<()> {
     let args = std::env::args().collect::<Vec<String>>();
     let user_input = UserInput::from_user_input(args)?;
 
-    if let Err(e) = begin_processing(user_input).await {
-        eprintln!("Error: {}", e);
-    }
+    let start = tokio::time::Instant::now();
+    if let Err(e) = tokio::join!(begin_processing(user_input)).0 {
+        eprintln!("Error: {e}");
+    };
+
+    let duration = start.elapsed();
+    println!("->> {:<12} - TOTAL TIME", duration.as_secs_f64());
+
+    // if let Err(e) = begin_processing(user_input).await {
+    //     eprintln!("Error: {e}");
+    // }
 
     Ok(())
 }
 
 async fn begin_processing(input: UserInput) -> Result<()> {
-    let processor = Box::new(file_handling::Processor::new(&input));
+    let processor = file_handling::Processor::new(&input);
     println!("->> {:<12} -  processor created", "BEGIN_PROCESSING");
 
-    let mover = Box::new(file_handling::Mover::new(&input));
+    let mover = file_handling::Mover::new(&input);
     println!("->> {:<12} - mover created", "BEGIN_PROCESSING");
 
-    let files = processor.process_dir().await?;
+    let files = processor.process().await?;
     println!("->> {:<12} - DIR PROCESSED", "LAST STAGE - MOVE");
 
-    mover
-        .move_files(&files.iter().map(PathBuf::from).collect::<Vec<PathBuf>>())
-        .await?;
+    mover.par_move_files_copy(files.as_slice()).await?;
+    println!("->> {:<12} - MOVE PROCESSED", "COMPLETED");
 
     Ok(())
 }
